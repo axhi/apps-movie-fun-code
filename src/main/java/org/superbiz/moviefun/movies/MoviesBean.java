31d9564e10b6b6be7e41deacb03b004600accbe7
Joey Sabani <asabani@pivotal.io>
2018-01-24 13:54:51 -0700

    Move to spring boot

--- src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
@@ -19,6 +19,7 @@ package org.superbiz.moviefun.movies;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -26,7 +27,7 @@ import javax.persistence.criteria.*;
 import javax.persistence.metamodel.EntityType;
 import java.util.List;
 
-@Repository
+@Stateless
 public class MoviesBean {
 
     @PersistenceContext
@@ -36,22 +37,18 @@ public class MoviesBean {
         return entityManager.find(Movie.class, id);
     }
 
-    @Transactional
     public void addMovie(Movie movie) {
         entityManager.persist(movie);
     }
 
-    @Transactional
     public void editMovie(Movie movie) {
         entityManager.merge(movie);
     }
 
-    @Transactional
     public void deleteMovie(Movie movie) {
         entityManager.remove(movie);
     }
 
-    @Transactional
     public void deleteMovieId(long id) {
         Movie movie = entityManager.find(Movie.class, id);
         deleteMovie(movie);
